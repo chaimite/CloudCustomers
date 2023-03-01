1a16a49af19611a75caf69cf7cf52a413de5c54a
Delfina Correia <delfinacorreia@Mini-de-Jose.Home>
2023-03-01 16:21:51 +0000

    chore(tests): added test project

---
+++ CloudCustomers.UnitTests/Systems/Controllers/TestUserController.cs
@@ -0,0 +1,18 @@
+
+using CloudCustomers.API.Controllers;
+
+namespace CloudCustomers.UnitTests;
+
+public class TestUserController
+{
+    [Fact]
+    public void Get_OnSuccess_ReturnsStatusCode200()
+    {
+        //Arrange
+        var sut = new UserController();
+
+        //Act
+
+        //Assert       
+    }
+}
--- CloudCustomers/Controllers/UserController.cs
+++ CloudCustomers/Controllers/UserController.cs
@@ -4,30 +4,20 @@ namespace CloudCustomers.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class UserController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
 
-    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<UserController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public UserController(ILogger<UserController> logger)
     {
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet(Name = "GetUser")]
+    public async Task<IActionResult> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return null;
     }
 }
 
