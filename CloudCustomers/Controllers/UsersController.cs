f5c9424d5754c4df1803ca65828a44fd8da46cc2
Delfina Correia <delfinacorreia@Mini-de-Jose.Home>
2023-03-03 16:30:30 +0000

    task(tests): added services and models folders to project

---
+++ CloudCustomers/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using CloudCustomers.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudCustomers.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _usersService;
+
+    public UsersController(IUserService usersService)
+    {
+        _usersService = usersService;
+    }
+
+    [HttpGet(Name = "GetUsers")]
+    public async Task<IActionResult> Get()
+    {
+        
+        var users = await _usersService.GetAllUsers();
+        if (users.Any()) {
+            return Ok(users);
+        }
+        return NotFound();
+    }
+}
+
