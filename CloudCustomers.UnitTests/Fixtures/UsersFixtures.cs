40a797bb64621f86cd7b1178c91729528c5545b1
Delfina Correia <delfinacorreia@Mini-de-Jose.Home>
2023-03-03 17:25:55 +0000

    task(fixture): added mock data for list of users

--- CloudCustomers.UnitTests/Fixtures/UsersFixtures.cs
+++ CloudCustomers.UnitTests/Fixtures/UsersFixtures.cs
@@ -5,18 +5,33 @@ namespace CloudCustomers.UnitTests.Fixtures
 {
     public static class UsersFixtures
     {
-        public static List<User> GetTestUsers() => new()
+        public static List<User> GetTestUsers() =>
+        new()
         {
-            new User{
-            Name = "",
-            Id=1,
-            Email = "",
-            Address = new Address
+            new User
             {
-                City= "",
-                Street = "",
-                ZipCode = ""
-            } }
+                Name = "Test user",
+                Id = 1,
+                Email = "testuser1@gmail.com",
+                Address = new Address
+                {
+                    City= "Some city",
+                    Street = "Some street",
+                    ZipCode = "12345"
+                }
+            },
+            new User
+            {
+                Name = "Test user2",
+                Id = 2,
+                Email = "testuser2@gmail.com",
+                Address = new Address
+                {
+                    City= "Some city",
+                    Street = "Some street",
+                    ZipCode = "12345"
+                }
+            }
         };	
 	}
 }
